295e0a8803beeba85b46576be40dc9b37f6e39eb
yuki yamashita <yuki.baystars4580@gmail.com>
2022-11-16 15:38:10 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get 'mypages/index' => 'mypages#index'
+end
